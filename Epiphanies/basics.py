537e8310a3df5e241b9be58bb99e8f55915e6680
Bejoy Mathew <bejoygm@gmail.com>
2017-03-26 13:50:11 +0530

    added names examples

---
+++ Epiphanies/basics.py
@@ -0,0 +1,41 @@
+print(True.__class__)
+print(True.__class__.__bases__)
+
+print(bool.__mro__)
+
+import inspect
+print(inspect.getmro(type(True)))
+
+# dir works on instances too
+print(dir(5))
+
+print(dir()) # what's in current namespace
+
+# Imp Note:
+# Simple name assignment and re-assignment are not operations on objects,
+# they are namespace operations!
+
+a = 400
+b = a
+
+assert id(a) == id(b)
+
+del a # only removes 'a' namespace not the object it referes to
+
+a = 1
+b = 1
+print(a is b) # True
+
+a = 500
+b = 500
+print(a is b) # False
+
+# it is because of CPython implementation, some ints are pre-created
+
+# range of precreated numbers
+import itertools
+for i in itertools.chain(range(-7, -3), range(254, 259)):
+    print(i, id(i))
+
+
+
