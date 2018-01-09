3308f735107c0c18a009653f72b8608a854eeedc
Bejoy Mathew <bejoygm@gmail.com>
2018-01-09 15:39:52 +0530

    added attribute lookup exmaple for special methods

---
+++ objects/loggingproxy.py
@@ -0,0 +1,36 @@
+class LoggingProxy:
+
+    def __init__(self, target):
+        super().__setattr__('target', target)
+
+    def __getattribute__(self, name):
+        target = super().__getattribute__('target')
+
+        try:
+            value = getattr(target, name)
+        except AttributeError as e:
+            raise AttributeError("{} could not forward request {} to {}".format(
+                super().__getattribute__('__class__').__name__,
+                name,
+                target)) from e
+        print("Retrieved attribute {!r} = {!r} from {!r}".format(name, value, target))
+        return target
+
+    def __setattr__(self, name, value):
+        target = super().__getattribute__('target')
+
+        try:
+            setattr(target, name, value)
+        except AttributeError as e:
+            raise AttributeError("{} could not forward request {} to {}".format(super().__getattribute__('__class__').__name,
+                name,
+                target)) from e
+        print("Set attribute {!r} = {!r} on {!r}".format(name, value, target))
+
+    def __repr__(self):
+        target = super().__getattribute__('target')
+        repr_callable = getattr(target, '__repr__')
+        return repr_callable()
+
+
+
