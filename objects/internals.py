3da51120b9a9c2ab0c67fb906aae5482fa7b3333
Bejoy Mathew <bejoygm@gmail.com>
2018-01-09 12:52:33 +0530

    get set delete attribute override

--- objects/internals.py
+++ objects/internals.py
@@ -5,6 +5,22 @@ class Vector:
         private_coords = {'_' + k: v for k, v in coords.items()}
         self.__dict__.update(private_coords)
 
+    def __getattr__(self, name):
+        private_name = '_' + name
+        # EAFP style programming
+        try:
+            return self.__dict__[private_name]
+        except KeyError:
+            if private_name not in self.__dict__:
+                raise AttributeError('{!r} object has not attributes {!r}'.format(
+                        self.__class__, name))
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Can't set attribute {!r}".format(name))
+
+    def __delattr__(self, name):
+        raise AttributeError("Can't delete attribute {!r}".format(name))
+
     def __repr__(self):
         # slice string for removing _ while printing
         return "{}({})".format(
